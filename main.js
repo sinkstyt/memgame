9ac3887da0895216a331d0ca02d0bfd189d368b1
Tyler Sinks <tyler.sinks@gmail.com>
2020-03-26 11:31:14 -0400

    updated the working files to the correct local directory

---
+++ main.js
@@ -0,0 +1,24 @@
+// Now you'll be working to create the cards that will make up your memory game. Each card will have a value of "queen" or "king", and there should be a total of four cards. You'll also push your code up to your repository.
+
+// Create four variables that simulate the cards in your game.
+
+//     They should be string types.
+//     Two should have a value of "queen", and the other two should have a value of "king".
+
+//     They will represent your cards (e.g., "queen" card).
+//     Name your variables cardOne, cardTwo, cardThree, and cardFour.
+
+// My first thoughts about coding this game went the object route:
+// let cards = {
+//     cardOne : "queen"
+//     cardTwo : "queen"
+//     cardThree : "king"
+//     cardFour : "king"
+// };
+
+let cardOne = "queen";
+let cardTwo = "queen";
+let cardThree = "king";
+let cardFour = "king";
+console.log("User flipped " + cardOne);
+console.log("User flipped " + cardTwo);
